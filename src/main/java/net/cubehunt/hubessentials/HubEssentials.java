87d5ef7ab1eac5f504749c4ac65c4ffc813666c4
LuuckA21 <luca.dalu@outlook.it>
2022-07-09 12:02:41 +0200

    Initial commit

---
+++ src/main/java/net/cubehunt/hubessentials/HubEssentials.java
@@ -0,0 +1,17 @@
+package net.cubehunt.hubessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HubEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
