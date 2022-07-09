6db163f58c1fe819160f77bda6a397eccf43dff7
LuuckA21 <luca.dalu@outlook.it>
2022-07-09 12:39:53 +0200

    pom.xml initial setup

--- src/main/java/net/cubehunt/hubessentials/HubEssentials.java
+++ src/main/java/net/cubehunt/hubessentials/HubEssentials.java
@@ -2,11 +2,17 @@ package net.cubehunt.hubessentials;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class HubEssentials extends JavaPlugin {
+
+    private static final Logger LOGGER = Logger.getLogger("HubEssentials");
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        if (LOGGER != super.getLogger()) {
+            LOGGER.setParent(super.getLogger());
+        }
 
     }
 
